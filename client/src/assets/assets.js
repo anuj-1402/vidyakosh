2b31daaabee719923232c4fbf7b7f3af0d2aa2fd
anuj <apal83205@gmail.com>
2025-08-09 19:36:18 +0530

    Add VidyaKosh logo to assets and update Navbar to use it

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -42,9 +42,11 @@ import profile_img from './profile_img.png'
 import profile_img2 from './profile_img2.png'
 import profile_img3 from './profile_img3.png'
 import lesson_icon from './lesson_icon.svg'
+import VidyaKoshlogo from './VidyaKoshlogo.svg';
 
 
 export const assets = {
+    VidyaKoshlogo,
     logo,
     search_icon,
     sketch,
