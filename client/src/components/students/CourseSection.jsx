0c2d2004cca8f950a126611cd97bcd31fa6f192c
anuj <apal83205@gmail.com>
2025-08-09 21:53:35 +0530

    Add CourseSection component to Home page and enhance its content

--- client/src/components/students/CourseSection.jsx
+++ client/src/components/students/CourseSection.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const CourseSection = () => {
-  return <div>CourseSection</div>;
+  return <div className="py-16 md:px-40 px-8">
+<h2 className='text-3xl font-medium text-gray-800'> Learn From The Best </h2>
+<p className='text -sm md:text-base text-gray-500 mt-3'> Discover Our top-notch courses and elevate your skills to new heights. From Coding Bootcamps to Data Science programs, we have something for everyone. </p>
+<Link to={"/course-list"} onClick={() => scrollTo(0, 0)} className="text-gray-500 border border-gray-500/30 py-3 rounded mt-8 block w-60 mx-auto text-center">Browse All Courses</Link>
+  </div>;
 };
 
 export default CourseSection;
