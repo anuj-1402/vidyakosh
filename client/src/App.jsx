c835381e9c90569986564789fbc99cc7ee486881
anuj <apal83205@gmail.com>
2025-08-09 17:28:44 +0530

    Initial commit: client setup

---
+++ client/src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Home from './pages/students/Home.jsx'
+import CourseList from './pages/students/CourseList'
+import CourseDetails from './pages/students/CourseDetails.jsx'
+import MyEnrollments from './pages/students/MyEnrollments'
+import { Route, Routes, useMatch } from 'react-router-dom'
+import Player from './pages/students/Player'
+import Loading from './components/students/Loading'
+import Educator from './pages/educator/Educator'
+import Dashboard from './pages/educator/Dashboard'
+import AddCourse from './pages/educator/AddCourse'
+import MyCourses from './pages/educator/MyCourses'
+import StudentsEnrolled from './pages/educator/StudentsEnrolled'
+import Navbar from './components/students/Navbar.jsx'
+
+export default function App() {
+  const isEducatorRoute=useMatch('/educator/*');
+  return (
+    <div className='text-default min-h-screen bg-white '>
+      {!isEducatorRoute && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/course-list" element={<CourseList />} />
+        <Route path="/course-list/:input" element={<CourseList />} />
+        <Route path="/course/:id" element={<CourseDetails />} />
+      <Route path="/player/:courseId" element={<Player />} />
+      <Route path="/my-enrollments" element={<MyEnrollments />} />
+      <Route path="/loading/:path" element={<Loading />} />
+    <Route path="/educator" element={<Educator />}>
+      <Route path="educator" element={<Dashboard />} />
+      <Route path="add-course" element={<AddCourse />} />
+      <Route path="my-courses" element={<MyCourses />} />
+      <Route path="students-enrolled" element={<StudentsEnrolled />} />
+    </Route>
+      </Routes>
+    </div>
+  );
+}
---
+++ client/src/components/students/TestimonalsSection.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TestimonalsSection = () => {
+  return <div>TestimonalsSection</div>;
+};
+
+export default TestimonalsSection;
