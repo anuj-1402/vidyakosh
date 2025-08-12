807b94f7d740cba0778f491e1278cbc7c66cb038
anuj <apal83205@gmail.com>
2025-08-12 22:35:05 +0530

    Refactor educator components for improved structure and maintainability

--- client/src/components/educator/Navbar.jsx
+++ client/src/components/educator/Navbar.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { assets } from "../../assets/assets";
+import { dummyEducatorData } from "../../assets/assets";
+import { UserButton,useUser } from "@clerk/clerk-react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const educatorData = dummyEducatorData;
+  const user = useUser();
+
+  return <div className="flex items-center justify-between p-x4 md:px-8 border-b border-gray-500 py-3">
+    <Link to="/">
+      <img src={assets.VidyaKoshlogo} alt="Logo" className="w-28 lg:w-32" />
+    </Link>
+  <div className="flex items-center gap-5 text-gray-500 relative">
+    <p> Hi { user ? user.fullName : "Developers"}</p>
+    {user ? <UserButton /> : <img src={assets.profile_img} alt="Default User" className="max-w-8" />}
+  </div>
+  
+  </div>;
 };
 
 export default Navbar;
--- client/src/components/educator/Sidebar.jsx
+++ client/src/components/educator/Sidebar.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import { assets } from "../../assets/assets";
+import { AppContext } from "../../context/AppContext";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const menuItems = [
+    { name: "Dashboard", path: "/educator", icon: <img src={assets.home_icon} alt="Dashboard" /> },
+    { name: "Add Course", path: "/educator/add-course", icon: <img src={assets.add_icon} alt="Add Course" /> },
+    { name: "My Courses", path: "/educator/my-courses", icon: <img src={assets.my_course_icon} alt="My Courses" /> },
+    { name: "Students Enrolled", path: "/educator/student-enrolled", icon: <img src={assets.person_tick_icon} alt="Students Enrolled" /> },
+  ];
+  const { isEducator } = useContext(AppContext);
+  return  isEducator &&<div  className="md:w-64 w-16 border-r min-h-screen text-base border-gray-500 py-2 flex flex-col">
+    
+
+  </div>;
 };
 
 export default Sidebar;
